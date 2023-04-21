ab900abd7eb653c1114541cca084de0f5280aad9
Marina <mbakanova2016@yandex.ru>
2023-04-22 00:06:53 +0300

    Third person controller fixed

--- Assets/Scripts/Movement/MovemenMethod.cs
+++ Assets/Scripts/Movement/MovemenMethod.cs
@@ -4,7 +4,7 @@ namespace Movement
 {
     public class MovemenMethod : MonoBehaviour
     {
-        [SerializeField] private float _moveSpeed = 10f;
+        private const float MoveSpeed = 10f;
         [SerializeField] private Transform _cameraTransform;
 
         private void FixedUpdate()
@@ -18,7 +18,7 @@ namespace Movement
             var vertical = Input.GetAxis("Vertical");
             var movement = _cameraTransform.right * horizontal + _cameraTransform.forward * vertical;
             movement.y = 0f;
-            transform.position += movement * (_moveSpeed * Time.deltaTime);
+            transform.position += movement * (MoveSpeed * Time.deltaTime);
         }
     }
 }
--- Assets/Scripts/Movement/ThirdPersonController.cs
+++ Assets/Scripts/Movement/ThirdPersonController.cs
@@ -1,33 +1,35 @@
-using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Movement
 {
     public class ThirdPersonController : MonoBehaviour
     {
-        public Transform _target;
-        [SerializeField] private float _distance = 5f;
-        [SerializeField] private float _height = 2f;
-        [SerializeField] private float _rotationSpeed = 5f;
-        private float _currentRotationX = 0f;
-        private float _currentRotationY = 0f;
+        [SerializeField] private Transform _target;
+        private const float Distance = 5f;
+        private const float Height = 2f;
+        private const float Sensitivity = 5f;
+        private float _currentRotationX;
+        private float _currentRotationY;
+        private float _mouseX;
+        private float _mouseY;
         private Vector3 _offset;
-
-        private void Start()
-        {
-            _offset = (transform.position - _target.position);
-        }
 
         private void LateUpdate()
         {
-            _currentRotationX += Input.GetAxis("Mouse X") * _rotationSpeed;
-            _currentRotationY -= Input.GetAxis("Mouse Y") * _rotationSpeed;
+            _mouseX = Input.GetAxis("Mouse X") * Sensitivity;
+            _mouseY = Input.GetAxis("Mouse Y") * Sensitivity;
+            _currentRotationX += _mouseX;
+            _currentRotationY -= _mouseY;
             _currentRotationY = Mathf.Clamp(_currentRotationY, -90f, 90f);
-            var rotation = Quaternion.Euler(_currentRotationY, _currentRotationX, 0f);
-            //Vector3 position = rotation * new Vector3(0f, height, -distance) + target.position;
-            transform.rotation = rotation;
-            transform.position = _target.position + _offset;
+
+            var rotation = Quaternion.Euler(0f, _currentRotationX, 0f);
+            _offset = new Vector3(0f, Height, -Distance);
+            _offset = rotation * _offset;
+
+            var targetPosition = _target.position;
+            transform.position = targetPosition + _offset;
+            transform.LookAt(targetPosition);
+            transform.rotation = Quaternion.Euler(_currentRotationY, _currentRotationX, 0f);
         }
     }
 }
