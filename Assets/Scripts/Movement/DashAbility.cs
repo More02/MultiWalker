3139cc9cfcf86cfe05b4630972508b0381953f46
Marina <mbakanova2016@yandex.ru>
2023-04-24 01:06:59 +0300

    Change colors

--- Assets/Scripts/Movement/DashAbility.cs
+++ Assets/Scripts/Movement/DashAbility.cs
@@ -10,7 +10,9 @@ namespace Movement
     {
         private Rigidbody _rigidbody;
         private readonly Color _dashedColor = new Color((float)0.6226415, (float)0.2196867, (float)0.2196867);
+
         private readonly Color _baseColor = Color.white;
+        
         [SerializeField] private float _distance = 5f;
         [SerializeField] private float _disabledTime = 3f;
         private Component[] _skinnedMeshRenderers;
@@ -24,6 +26,8 @@ namespace Movement
         private Transform _cameraTransform;
         private Transform _collisionRoot;
 
+        private bool _state;
+
         private IEnumerator _dashCoroutine;
 
         public bool IsDashing { get; private set; }
@@ -33,6 +37,7 @@ namespace Movement
         private void Start()
         {
             Instance = this;
+            _skinnedMeshRenderers = GetComponentsInChildren(typeof(SkinnedMeshRenderer), true);
             _rigidbody = gameObject.GetComponent<Rigidbody>();
             _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
         }
@@ -45,6 +50,7 @@ namespace Movement
 
         private void Update()
         {
+            
             if (!isLocalPlayer) return;
             if (!Input.GetMouseButtonDown(0)) return;
             if (IsDashing) return;
@@ -52,6 +58,11 @@ namespace Movement
             _startPosition = transform.position;
             _dashCoroutine = Dash();
             StartCoroutine(_dashCoroutine);
+        }
+
+        private void StateColorChange()
+        {
+            ChangeColor(!_state ? _baseColor : _dashedColor);
         }
 
         private async void OnCollisionEnter(Collision collision)
@@ -64,20 +75,51 @@ namespace Movement
             if (!IsDashing) return;
             var collisionDashAbility = _collisionRoot.GetComponent<DashAbility>();
             if (!collisionDashAbility._isAvailableForDash) return;
+            
+            CmdChangeState(collisionDashAbility, !collisionDashAbility._state);
+            
             collisionDashAbility._isAvailableForDash = false;
             _countOfSuccessDash++;
             Debug.Log(_countOfSuccessDash);
             StopCoroutine(_dashCoroutine);
             IsDashing = false;
+            await DelayTask();
+            CmdChangeState(collisionDashAbility, !collisionDashAbility._state);
+            collisionDashAbility._isAvailableForDash = true;
             // _rigidbody.velocity = Vector3.zero;
             // _collisionRoot.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            _skinnedMeshRenderers = _collisionRoot.GetComponentsInChildren(typeof(SkinnedMeshRenderer), true);
-            _collisionRoot.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(ChangeColor());
+            // _skinnedMeshRenderers = _collisionRoot.GetComponentsInChildren(typeof(SkinnedMeshRenderer), true);
+            //_nowColor = _dashedColor;
+            //_collisionRoot.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(ChangeColor());
             // CmdChangeColor(_dashedColor);
-            // await DelayTask();
+
             // CmdChangeColor(_baseColor);
             // collisionDashAbility._isAvailableForDash = true;
         }
+
+        private static void ChangeState(DashAbility collisionDashAbility, bool state)
+        {
+            collisionDashAbility._state = state;
+            collisionDashAbility.StateColorChange();
+        }
+
+        [Command(requiresAuthority = false)]
+        private void CmdChangeState(DashAbility collisionDashAbility, bool state)
+        {
+            RpcChangeState(collisionDashAbility, state);
+            ChangeState(collisionDashAbility, state);
+        }
+        [ClientRpc]
+        private void RpcChangeState(DashAbility collisionDashAbility, bool state)
+        {
+            ChangeState(collisionDashAbility, state);
+        }
+        
+        // private void OnColorChanged(Color oldColor, Color newColor)
+        // {
+        //     if (!isLocalPlayer) return;
+        //     _collisionRoot.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(ChangeColor());
+        // }
 
         private IEnumerator Dash()
         {
@@ -107,13 +149,9 @@ namespace Movement
         // }
 
 
-        private IEnumerator ChangeColor()
+        private IEnumerator CoroutineChangeColors()
         {
-            foreach (var component in _skinnedMeshRenderers)
-            {
-                var skinnedMeshRenderer = (SkinnedMeshRenderer)component;
-                skinnedMeshRenderer.materials[0].SetColor(_color, _dashedColor);
-            }
+            ChangeColor(_dashedColor);
 
             yield return new WaitForSeconds(_disabledTime);
             if (_countOfSuccessDash == 3)
@@ -121,27 +159,10 @@ namespace Movement
                 Debug.Log("Win");
             }
 
-            foreach (var component in _skinnedMeshRenderers)
-            {
-                var skinnedMeshRenderer = (SkinnedMeshRenderer)component;
-                skinnedMeshRenderer.materials[0].SetColor(_color, _baseColor);
-            }
+            ChangeColor(_baseColor);
 
             _collisionRoot.GetComponent<DashAbility>()._isAvailableForDash = true;
-        }
-
-
-        [Command(requiresAuthority = false)]
-        private void CmdChangeColor(Color color)
-        {
-            RpcChangeColor(color);
-            ChangeColor(color);
-        }
-
-        [ClientRpc]
-        private void RpcChangeColor(Color color)
-        {
-            ChangeColor(color);
+            //_nowColor = Color.clear;
         }
 
         private void ChangeColor(Color color)
@@ -153,9 +174,44 @@ namespace Movement
             }
         }
 
+
+        // [Command(requiresAuthority = false)]
+        // private void CmdChangeColor(Color color)
+        // {
+        //     RpcChangeColor(color);
+        //     StartCoroutine(CoroutineChangeColors());
+        // }
+
+        // [Command(requiresAuthority = false)]
+        // private void CmdChangeColor()
+        // {
+        //     StartCoroutine(ChangeColor());
+        // }
+
+        // [ClientRpc]
+        // private void RpcChangeColor()
+        // {
+        //     StartCoroutine(ChangeColor());
+        // }
+
+        // [ClientRpc]
+        // private void RpcChangeColor(Color color)
+        // {
+        //     StartCoroutine(CoroutineChangeColors());
+        // }
+
+        // private void ChangeColor(Color color)
+        // {
+        //     foreach (var component in _skinnedMeshRenderers)
+        //     {
+        //         var skinnedMeshRenderer = (SkinnedMeshRenderer)component;
+        //         skinnedMeshRenderer.materials[0].SetColor(_color, color);
+        //     }
+        // }
+
         private async Task DelayTask()
         {
-            await Task.Delay((int)_disabledTime);
+            await Task.Delay(3000);
         }
     }
 }
