2917952cb392e3f30c5fdb9eb176649e0871d6e9
Marina <mbakanova2016@yandex.ru>
2023-05-02 04:48:50 +0300

    Start client

--- Assets/Scripts/Player/NetworkPlayerManager.cs
+++ Assets/Scripts/Player/NetworkPlayerManager.cs
@@ -1,16 +1,23 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Mirror;
 using Spawn;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Player
 {
     public class NetworkPlayerManager : NetworkManager
     {
-        public float sceneRestartDelay = 5f;
+        private const float SceneRestartDelay = 5f;
+        private readonly List<NetworkConnection> _clientConnections = new();
+
         public override void OnServerAddPlayer(NetworkConnectionToClient conn)
         {
             InstantiatePlayer(conn);
+            _clientConnections.Add(conn);
         }
 
         private void InstantiatePlayer(NetworkConnectionToClient conn)
@@ -18,26 +25,43 @@ namespace Player
             var randomPlace = Random.Range(0, SpawnPointsHolder.Instanse.AllSpawnPoints.Count - 1);
             var player = Instantiate(playerPrefab, SpawnPointsHolder.Instanse.AllSpawnPoints[randomPlace].position,
                 SpawnPointsHolder.Instanse.AllSpawnPoints[randomPlace].rotation);
-            player.name = $"{playerPrefab.name} {conn.connectionId+1}";
+            player.name = $"{playerPrefab.name} {conn.connectionId + 1}";
             NetworkServer.AddPlayerForConnection(conn, player.gameObject);
             SpawnPointsHolder.Instanse.AllSpawnPoints.RemoveAt(randomPlace);
         }
-        
+
+        public void EndOfGameSession()
+        {
+            StartCoroutine(RestartSceneDelayed());
+        }
 
         private IEnumerator RestartSceneDelayed()
         {
-            yield return new WaitForSeconds(sceneRestartDelay);
+            yield return new WaitForSeconds(SceneRestartDelay);
+
+            var networkManager = gameObject.GetComponent<NetworkManager>();
+            if (networkManager is null)
+            {
+                yield break;
+            }
             
             foreach (var conn in NetworkServer.connections.Values)
             {
                 conn.Disconnect();
             }
-            singleton.ServerChangeScene(NetworkManager.singleton.onlineScene);
-        }
-        
-        public void EndOfGameSession()
-        {
-            StartCoroutine(RestartSceneDelayed());
+
+            // networkManager.StopServer();
+            // networkManager.StartServer();
+
+            const string host = "localhost";
+            const int port = 8080;
+            foreach (var uri in _clientConnections.Select(conn => $"tcp://{host}:{port}/{conn.connectionId}").Select(url => new Uri(url)))
+            {
+                networkManager.StartClient(uri);
+            }
+            
+            singleton.ServerChangeScene(singleton.onlineScene);
+            _clientConnections.Clear();
         }
     }
 }
